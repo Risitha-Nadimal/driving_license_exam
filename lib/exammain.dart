3e83f8f0fdf06c7a9abe57a97aeb71cc1130e612
Risitha Nadimal <102137397+Risitha-Nadimal@users.noreply.github.com>
2025-05-17 23:39:57 +0530

    exammain finish

--- lib/exammain.dart
+++ lib/exammain.dart
@@ -9,97 +9,175 @@ class MockExamScreen extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: const Color(0xFFF4FAFF),
-      body: Column(
-        children: [
-          appbar(
-              size: size,
-              bgcolor: const Color(0xff4378DB),
-              textColor: const Color.fromARGB(255, 255, 255, 255),
-              heading: "MOCK EXAM"),
-          Center(
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 400),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Banner Image
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      'assets/images/mockexam.png', // replace with actual image path
-                      height: 160,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            appbar(
+                size: size,
+                bgcolor: const Color(0xff4378DB),
+                textColor: const Color.fromARGB(255, 255, 255, 255),
+                heading: "MOCK EXAM"),
+            Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: size.width * 0.9),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Banner Image
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        'assets/images/mockexam.png', // replace with actual image path
+                        height: size.height * 0.25,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-                  // Exam Information
-                  Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    elevation: 2,
-                    child: const Padding(
-                      padding: EdgeInsets.all(16),
+                    // Exam Information
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(16),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          const Text(
+                            'Mock Exam Information',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 12),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              InfoTile(
-                                  label: 'Test Course', value: 'Sri Lanka'),
-                              InfoTile(label: 'Duration', value: '60 Minutes'),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: const Color(0xffCCE7FE),
+                                    ),
+                                    child: const Icon(Icons.question_mark,
+                                        color: Color.fromARGB(255, 0, 0, 0)),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  const InfoTile(
+                                      label: 'Total Question',
+                                      value: '50 \n Questions'),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: const Color(0xffCCE7FE),
+                                    ),
+                                    child: const Icon(Icons.access_time_rounded,
+                                        color: Color.fromARGB(255, 0, 0, 0)),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  const InfoTile(
+                                      label: 'Duration', value: '60 Minutes'),
+                                ],
+                              ),
                             ],
                           ),
-                          SizedBox(height: 12),
+                          const SizedBox(height: 12),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              InfoTile(label: 'Passing Score', value: '45/50'),
-                              InfoTile(label: 'Attempts', value: 'Unlimited'),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: const Color(0xffCCE7FE),
+                                    ),
+                                    child: const Icon(Icons.check_circle,
+                                        color: Color.fromARGB(255, 0, 0, 0)),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  const InfoTile(
+                                      label: 'Passing Score',
+                                      value: '45/50\n(86%)'),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: const Color(0xffCCE7FE),
+                                    ),
+                                    child: const Icon(Icons.replay,
+                                        color: Color.fromARGB(255, 0, 0, 0)),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  const InfoTile(
+                                      label: 'Attempts', value: 'Unlimited'),
+                                ],
+                              ),
                             ],
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-                  const Text(
-                    'Available Mock Tests',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10),
+                    const Text(
+                      'Available Mock Tests',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
 
-                  // Exam Paper 1
-                  ExamCard(
-                    title: 'Exam Paper 1',
-                    questions: '30 Questions',
-                    duration: '60 Minutes',
-                    description:
-                        'Covers traffic signs, road rules, and basic driving.',
-                    onPressed: () {
-                      // Start Exam 1 logic
-                    },
-                  ),
-                  const SizedBox(height: 10),
+                    // Exam Paper 1
+                    ExamCard(
+                      title: 'Exam Paper 1',
+                      questions: '30 Questions',
+                      duration: '60 Minutes',
+                      description:
+                          'Covers traffic signs, road rules, and basic driving regulations.',
+                      onPressed: () {
+                        // Start Exam 1 logic
+                      },
+                    ),
+                    const SizedBox(height: 10),
 
-                  // Exam Paper 2
-                  ExamCard(
-                    title: 'Exam Paper 2',
-                    questions: '30 Questions',
-                    duration: '60 Minutes',
-                    description:
-                        'Covers signal interpretation, road rules, and more.',
-                    onPressed: () {
-                      // Start Exam 2 logic
-                    },
-                  ),
-                ],
+                    // Exam Paper 2
+                    ExamCard(
+                      title: 'Exam Paper 2',
+                      questions: '35 Questions',
+                      duration: '60 Minutes',
+                      description:
+                          'Covers signal interpretation, road rules, and more.',
+                      onPressed: () {
+                        // Start Exam 2 logic
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -143,31 +221,52 @@ class ExamCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        side: BorderSide(color: Colors.blue.shade200, width: 2),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title,
+    return Container(
+      width: double.infinity, // Takes full screen width
+      margin:
+          const EdgeInsets.symmetric(vertical: 10), // Add some vertical spacing
+      child: Card(
+        color: const Color(0xFFF4FAFF),
+        elevation: 4, // Adds shadow
+        shadowColor: Colors.blue.withOpacity(0.5), // Custom shadow color
+        shape: RoundedRectangleBorder(
+          // side: BorderSide(color: Colors.blue.shade200, width: 2),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
                 style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 6),
-            Text('$questions  •  $duration',
-                style: const TextStyle(color: Colors.grey)),
-            const SizedBox(height: 6),
-            Text(description),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: onPressed,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-              child: const Text('Start Exam'),
-            ),
-          ],
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                '$questions  •  $duration',
+                style: const TextStyle(color: Colors.grey),
+              ),
+              const SizedBox(height: 6),
+              Text(description),
+              const SizedBox(height: 12),
+              SizedBox(
+                width: double.infinity, // Makes button full width
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF4378DB),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: const Text(
+                    'Start Exam',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
